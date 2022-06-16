ef9b92153d0d3647f6d45997d40de133c58f6cdc
Alkylaid <alkylaid@gmail.com>
2022-06-15 22:34:44 -0500

    Create basic calculator functions

---
+++ calculator.js
@@ -0,0 +1,35 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    if (b === 0) return "nope";
+    return a / b;
+}
+
+function operate(a, b, operator) {
+    switch(operator) {
+        case "add" :
+            return add(a, b);
+            break;
+        case "subtract":
+            return subtract(a, b);
+            break;
+        case "multiply":
+            return multiply(a, b);
+            break;
+        case "divide":
+            return divide(a, b);
+            break;
+    }
+}
+
+console.log(operate(1,2,"divide"))
