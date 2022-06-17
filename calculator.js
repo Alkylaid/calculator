e7fc9f44756093735e15b0dbb95ef0793ec48182
Alkylaid <alkylaid@gmail.com>
2022-06-17 18:08:51 -0500

    Add ability to clear entry

--- calculator.js
+++ calculator.js
@@ -1,10 +1,16 @@
+let previousEntry = [];
 let currentEntry = [];
+let operator = "";
 const button = document.querySelectorAll('button');
+const entry = document.querySelector('#entry');
 
 button.forEach((btn) => {
     btn.addEventListener('click', () => {
-        updateDisplay(btn.getAttribute('value'));
-       
+        if (btn.getAttribute('value') === "clear") {
+            clear();
+        } else {
+            updateDisplay(btn.getAttribute('value'));
+        }
     })
 })
 
@@ -43,16 +49,24 @@ function operate(a, b, operator) {
 }
 
 function updateDisplay(input) {
-
-    const entry = document.querySelector('#entry');
-    if (/^[0-9/]+$/.test(input)) {
-            currentEntry.push(input);
-         }
-    if(input === "." && currentEntry[0] == null) {
+    if (input === "." && currentEntry[0] == null) {
         currentEntry.push("0");
         currentEntry.push(input);
-     } else if (input === "." && !currentEntry.includes(".")) {
+    } else if (input === "." && !currentEntry.includes(".")) {
         currentEntry.push(input);
+    } else if (/^[0-9/]+$/.test(input)) {
+        currentEntry.push(input);
+    } else {
+        operator = input;
+        previousEntry = currentEntry;
     }
     entry.textContent = currentEntry.join("").toString();
+}
+
+
+function clear() {
+    currentEntry = [];
+    entry.textContent = "0";
+    operator = "";
+    
 }
