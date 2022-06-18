694c4114752e9b0648f25f2b7489a80984207832
Alkylaid <alkylaid@gmail.com>
2022-06-18 03:19:23 -0500

    Fix operation and display functions

--- calculator.js
+++ calculator.js
@@ -1,22 +1,62 @@
-let previousEntry = [];
-let currentEntry = [];
-let operator = "";
+let previousEntry = ["0"];
+let currentEntry = ["0"];
+let operator = ["add", "subtract", "multiply", "divide"];
+let currentOperator = "";
 let operatorIsPressed = false;
 const button = document.querySelectorAll('button');
 const entry = document.querySelector('#entry');
 
 button.forEach((btn) => {
     btn.addEventListener('click', () => {
-        if (btn.getAttribute('value') === "clear") {
+        const input = btn.getAttribute('value');
+        if (input === "clear") {
             clear();
         }
-        if (btn.getAttribute('value') === "equals") {
-            operate(previousEntry, currentEntry, operator);
-        } else {
-            updateDisplay(btn.getAttribute('value'));
+        if (input === ".") {
+            addDecimal();
+        } 
+        if (input === "delete") {
+            currentEntry.pop();
         }
-    })
+        if (/^[0-9/]+$/.test(input)) {
+            if (operatorIsPressed) {
+                currentEntry = ["0"];
+                operatorIsPressed = false;
+            }
+            if (currentEntry[0] === "0" && currentEntry.length <= 1) {
+                currentEntry.pop();
+                
+            }
+                currentEntry.push(input);
+        }
+        if (operator.includes(input) && !currentOperator.length) {
+            previousEntry = currentEntry;
+            currentOperator = input;
+            operatorIsPressed  = true;
+            updateDisplay();
+         }  else if (operator.includes(input) && currentOperator.length) {
+            operatorIsPressed = true;
+            operate(previousEntry,currentEntry,currentOperator);
+            previousEntry = currentEntry;
+            updateDisplay();
+            currentOperator = input;
+         }
+         if (input == "equals") {
+            if(currentOperator.length) {
+            operate(previousEntry,currentEntry, currentOperator);
+            operatorIsPressed = false;
+            updateDisplay();
+            previousEntry = currentEntry;
+            currentOperator = "";
+            }
+
+        }
+         else { 
+            updateDisplay();
+         }
+    
 })
+});
 
 function add(a, b) {
     const answer = parseFloat(a.join("").toString()) + parseFloat(b.join("").toString());
@@ -44,7 +84,6 @@ function divide(a, b) {
 function operate(a, b, operator) {
     switch (operator) {
         case "add":
-            console.log(b);
             getAnswer(add(a, b));
             break;
         case "subtract":
@@ -59,45 +98,47 @@ function operate(a, b, operator) {
     }
 }
 
-function updateDisplay(input) {
-    if (operatorIsPressed) {
-        currentEntry = [];
-        operatorIsPressed = false;
-    }
-    if (input === "delete") {
-        currentEntry.pop();
-        if (!currentEntry.length) {
-            currentEntry.push("0");
-        }
-    } else if (input === "." && currentEntry[0] == null) {
-        currentEntry.push("0");
-        currentEntry.push(input);
-    } else if (input === "." && !currentEntry.includes(".")) {
-        currentEntry.push(input);
-    } else if (/^[0-9/]+$/.test(input)) {
-        currentEntry.push(input);
-    } else {
-        operator = input;
-        operatorIsPressed = true;
-        previousEntry = currentEntry;
-    }
+function updateDisplay() {
+    // if (operatorIsPressed) {
+    //     currentEntry = [];
+    //     operatorIsPressed = false;
+    // }
+    // if (/^[0-9/]+$/.test(input)) {
+    //     currentEntry.push(input);
+    // } else if (input === "." && !currentEntry.includes(".")) {
+    //     currentEntry.push(input);
+    // } else {
+    //     operate(previousEntry, currentEntry, operator);
+        
+    // }
+    // operate(previousEntry, currentEntry, operator);
     entry.textContent = currentEntry.join("").toString();
+    
 
 }
 
 
 function clear() {
-    previousEntry = [];
-    currentEntry = [];
-    operator = "";
+    previousEntry = ["0"];
+    currentEntry = ["0"];
     entry.textContent = "0";
+    operatorIsPressed = false;
+    currentOperator = "";
 
 }
 
 function getAnswer(answer) {
     currentEntry = [];
     currentEntry.push(answer);
-    operator = "";
-    entry.textContent = answer;
 
+}
+
+function addDecimal() {
+    if (operatorIsPressed) {
+        currentEntry = ["0"];
+    }
+    if (!currentEntry.includes(".")) {
+        currentEntry.push(".");
+        operatorIsPressed = false;
+    }
 }
